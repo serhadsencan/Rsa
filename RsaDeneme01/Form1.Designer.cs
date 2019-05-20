a3c939f5bb340834dd2d5f1c12318db1994110da
Emirhan Gümüş <emirhangms@gmail.com>
2019-05-21 01:23:46 +0300

    Try Carch Eklendi

--- RsaDeneme01/Form1.Designer.cs
+++ RsaDeneme01/Form1.Designer.cs
@@ -44,25 +44,28 @@
             // 
             // txtPlainText
             // 
-            this.txtPlainText.Location = new System.Drawing.Point(23, 35);
+            this.txtPlainText.Location = new System.Drawing.Point(31, 43);
+            this.txtPlainText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPlainText.Name = "txtPlainText";
-            this.txtPlainText.Size = new System.Drawing.Size(383, 125);
+            this.txtPlainText.Size = new System.Drawing.Size(509, 153);
             this.txtPlainText.TabIndex = 1;
             this.txtPlainText.Text = "";
             // 
             // txtCipherText
             // 
-            this.txtCipherText.Location = new System.Drawing.Point(26, 195);
+            this.txtCipherText.Location = new System.Drawing.Point(35, 240);
+            this.txtCipherText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCipherText.Name = "txtCipherText";
-            this.txtCipherText.Size = new System.Drawing.Size(385, 134);
+            this.txtCipherText.Size = new System.Drawing.Size(512, 164);
             this.txtCipherText.TabIndex = 2;
             this.txtCipherText.Text = "";
             // 
             // btnEncyrpt
             // 
-            this.btnEncyrpt.Location = new System.Drawing.Point(573, 196);
+            this.btnEncyrpt.Location = new System.Drawing.Point(607, 198);
+            this.btnEncyrpt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEncyrpt.Name = "btnEncyrpt";
-            this.btnEncyrpt.Size = new System.Drawing.Size(73, 49);
+            this.btnEncyrpt.Size = new System.Drawing.Size(97, 60);
             this.btnEncyrpt.TabIndex = 3;
             this.btnEncyrpt.Text = "Encyrpt";
             this.btnEncyrpt.UseVisualStyleBackColor = true;
@@ -70,9 +73,10 @@
             // 
             // btnDecyrpt
             // 
-            this.btnDecyrpt.Location = new System.Drawing.Point(662, 196);
+            this.btnDecyrpt.Location = new System.Drawing.Point(726, 198);
+            this.btnDecyrpt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDecyrpt.Name = "btnDecyrpt";
-            this.btnDecyrpt.Size = new System.Drawing.Size(73, 49);
+            this.btnDecyrpt.Size = new System.Drawing.Size(97, 60);
             this.btnDecyrpt.TabIndex = 4;
             this.btnDecyrpt.Text = "Decyrpt";
             this.btnDecyrpt.UseVisualStyleBackColor = true;
@@ -81,75 +85,83 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Location = new System.Drawing.Point(31, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(78, 17);
             this.label1.TabIndex = 5;
             this.label1.Text = "Plain Text :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 169);
+            this.label2.Location = new System.Drawing.Point(31, 208);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(84, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "Cipher Text:";
             // 
             // txt_P_val
             // 
-            this.txt_P_val.Location = new System.Drawing.Point(615, 70);
+            this.txt_P_val.Location = new System.Drawing.Point(666, 71);
+            this.txt_P_val.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_P_val.Name = "txt_P_val";
-            this.txt_P_val.Size = new System.Drawing.Size(100, 20);
+            this.txt_P_val.Size = new System.Drawing.Size(132, 22);
             this.txt_P_val.TabIndex = 7;
             // 
             // txt_Q_val
             // 
-            this.txt_Q_val.Location = new System.Drawing.Point(615, 106);
+            this.txt_Q_val.Location = new System.Drawing.Point(666, 115);
+            this.txt_Q_val.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Q_val.Name = "txt_Q_val";
-            this.txt_Q_val.Size = new System.Drawing.Size(100, 20);
+            this.txt_Q_val.Size = new System.Drawing.Size(132, 22);
             this.txt_Q_val.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(570, 70);
+            this.label3.Location = new System.Drawing.Point(606, 71);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.Size = new System.Drawing.Size(25, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "P :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(570, 106);
+            this.label4.Location = new System.Drawing.Point(606, 115);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.Size = new System.Drawing.Size(27, 17);
             this.label4.TabIndex = 10;
             this.label4.Text = "Q :";
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(23, 367);
+            this.txtMessage.Location = new System.Drawing.Point(31, 452);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(396, 130);
+            this.txtMessage.Size = new System.Drawing.Size(527, 159);
             this.txtMessage.TabIndex = 11;
             this.txtMessage.Text = "";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 341);
+            this.label5.Location = new System.Drawing.Point(36, 420);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.Size = new System.Drawing.Size(73, 17);
             this.label5.TabIndex = 12;
             this.label5.Text = "Message :";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 536);
+            this.ClientSize = new System.Drawing.Size(843, 641);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.label4);
@@ -162,8 +174,9 @@
             this.Controls.Add(this.btnEncyrpt);
             this.Controls.Add(this.txtCipherText);
             this.Controls.Add(this.txtPlainText);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RSA Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- RsaDeneme01/Rsa.cs
+++ RsaDeneme01/Rsa.cs
@@ -5,7 +5,7 @@
 
 public class Rsa
 {
-   
+
     private int n;
     private int N;
     private int e;
@@ -16,16 +16,16 @@ public class Rsa
     private byte[] last;
 
     public Rsa()
-	{
-       
-	}
-    
-   
+    {
+
+    }
+
+
     ////////
     public int gcd(int a, int b)
     {
         int gcd = 0;
-        while ((a%b)!=0)
+        while ((a % b) != 0)
         {
             gcd = a % b;
             a = b;
@@ -42,20 +42,20 @@ public class Rsa
 
         return mod;
     }
-    
+
 
     public void Gen_PublicKey(int P, int Q)
     {
-         n = P * Q;
-         N = (P-1) * (Q-1);
-       // Console.WriteLine("n == "+ n+ "  N == "+ N);
-      //  Console.WriteLine("e ::::"+e.ToString());
+        n = P * Q;
+        N = (P - 1) * (Q - 1);
+        // Console.WriteLine("n == "+ n+ "  N == "+ N);
+        //  Console.WriteLine("e ::::"+e.ToString());
         for (int i = 3; i < N; i++)
         {
             if (gcd(i, N) == 1)
             {
                 e = i;
-               // Console.WriteLine("E == " + e);
+                // Console.WriteLine("E == " + e);
                 break;
             }
 
@@ -66,67 +66,79 @@ public class Rsa
 
     public void Encyrpt(byte[] m)
     {
-       cipher = new int[m.Length];
-        
-        last = new byte[m.Length];
-        int temp = 1;
-      
-        for (int i = 0; i < m.Length; i++)
+        try
         {
-            
-            for (int j = 0; j < e; j++)
+            cipher = new int[m.Length];
+
+            last = new byte[m.Length];
+            int temp = 1;
+
+            for (int i = 0; i < m.Length; i++)
             {
-                temp = mod(temp * Convert.ToInt32(m[i]),n);
-                cipher[i] = temp;                
+
+                for (int j = 0; j < e; j++)
+                {
+                    temp = mod(temp * Convert.ToInt32(m[i]), n);
+                    cipher[i] = temp;
+                }
+
+                last[i] = Convert.ToByte(mod(cipher[i], 256));
+                temp = 1;
+
             }
-           
-            last[i] = Convert.ToByte(mod(cipher[i],256));            
-            temp = 1;
+
+            cipherText = System.Text.Encoding.UTF8.GetString(last);
 
         }
-
-        cipherText = System.Text.Encoding.UTF8.GetString(last);
-      
-        
+        catch (Exception)
+        {
+            throw;
+        }
     }
 
     public void Gen_PrivateKey()
     {
         d = 0;
-        while (mod((e*d),N)!=1)
+        while (mod((e * d), N) != 1)
         {
             d++;
         }
-       
-        
     }
 
-    
+
 
     public void Decyrpt()
     {
-        int temp = 1;
-        int[] tempArr = new int[last.Length];
-        
-       
-        for (int i = 0; i < last.Length; i++)
+        try
         {
-            
-            for (int j=0; j<d;j++)
-            {
-                temp = mod(temp* Convert.ToInt32(cipher[i]),n);
-              
-                tempArr[i] = temp;
-              
-            }
-            temp = 1;
-            
-            last[i] = Convert.ToByte(tempArr[i]%256);
-        }
-        plainText = System.Text.Encoding.UTF8.GetString(last);
-       
+            int temp = 1;
+            int[] tempArr = new int[last.Length];
 
-       
+
+            for (int i = 0; i < last.Length; i++)
+            {
+
+                for (int j = 0; j < d; j++)
+                {
+                    temp = mod(temp * Convert.ToInt32(cipher[i]), n);
+
+                    tempArr[i] = temp;
+
+                }
+                temp = 1;
+
+                last[i] = Convert.ToByte(tempArr[i] % 256);
+            }
+            plainText = System.Text.Encoding.UTF8.GetString(last);
+
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+
     }
-    
+
 }
